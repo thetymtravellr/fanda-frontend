121b65c97ff3fb8945222fb4b141ebce460f949f
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-07-10 16:48:26 +0600

    hero animation added

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,10 +1,31 @@
+import { motion } from "framer-motion";
 import React from "react";
 
-const Icons = ({ icon, className }) => {
+const Icons = ({ icon, className, left, duration }) => {
+  console.log(duration);
   return (
-    <div style={{ position: "absolute" }} className={className}>
-      <img src={icon} alt="" />
-    </div>
+    <>
+      {left ? (
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration }}
+          style={{ position: "absolute" }}
+          className={className}
+        >
+          <img src={icon} alt="" />
+        </motion.div>
+      ) : (
+        <motion.div
+          initial={{ x: 100}}
+          animate={{ x: 0 }}
+          style={{ position: "absolute" }}
+          className={className}
+        >
+          <img src={icon} alt="" />
+        </motion.div>
+      )}
+    </>
   );
 };
 
--- src/pages/HomePage/Hero.js
+++ src/pages/HomePage/Hero.js
@@ -6,14 +6,18 @@ import Icons from "../../components/Icons";
 
 const Hero = () => {
   return (
-    <section className="p-12">
+    <section className="p-4 md:p-12">
       <div className="content min-h-[40vh] w-full max-w-7xl mx-auto bg-teal-100 rounded-3xl relative grid place-content-center">
-        <Icons className="top-16 left-16 w-12" icon={cartIcon} />
-        <Icons className="top-12 right-20 w-12" icon={clothIcon} />
-        <Icons className="bottom-12 left-24 w-12" icon={saleIcon} />
-        <div className="w-full max-w-2xl mx-auto text-center">
-            <h1 className="text-4xl font-bold">Grab any 3 items and get 50% off! offer for limited time</h1>
-            <button className="mt-8 bg-black px-4 py-2 rounded-lg text-white">Shop now</button>
+        <Icons className="top-16 left-16 w-12" icon={cartIcon} left={true} duration={.5}/>
+        <Icons className="top-12 right-20 w-12" icon={clothIcon} duration={1}/>
+        <Icons className="bottom-12 left-24 w-12" icon={saleIcon} left={true} duration={.75}/>
+        <div className="w-full max-w-2xl mx-auto text-center z-10 lg:bg-black/0 bg-black/20 lg:backdrop-blur-none backdrop-blur-sm rounded-3xl p-8">
+          <h1 className="text-4xl font-bold">
+            Grab any 3 items and get 50% off! offer for limited time
+          </h1>
+          <button className="mt-8 bg-black px-4 py-2 rounded-lg text-white">
+            Shop now
+          </button>
         </div>
       </div>
     </section>
