d1bfef1e834e7fe21a814c261a8f67dba9e20ac7
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-07-25 20:44:54 +0600

    latest

--- src/pages/Store/Store.js
+++ src/pages/Store/Store.js
@@ -24,7 +24,7 @@ const Store = () => {
           <GrBaby className="text-2xl mx-auto mb-2" />
         </Category>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 justify-items-center">
           {products?.map((product) => (
             <Card product={product} />
           ))}
